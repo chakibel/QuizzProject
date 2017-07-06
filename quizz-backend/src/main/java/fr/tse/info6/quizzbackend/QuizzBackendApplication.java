9e2af3848f195a008c890d4d288e3f0469247957
jsubercaze <julien.subercaze@univ-st-etienne.fr>
2017-07-06 12:15:51 +0200

    initial source commit

---
+++ quizz-backend/src/main/java/fr/tse/info6/quizzbackend/QuizzBackendApplication.java
@@ -0,0 +1,12 @@
+package fr.tse.info6.quizzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzBackendApplication.class, args);
+	}
+}
---
+++ quizz-backend/src/test/java/fr/tse/info6/quizzbackend/QuizzBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.tse.info6.quizzbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizzBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
